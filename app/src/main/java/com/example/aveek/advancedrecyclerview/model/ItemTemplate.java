f921d0180fabbeead6cb24624035de38e378b7fa
Aveek <nsaveek@gmail.com>
2017-02-24 21:27:41 +0800

    data model preparation

---
+++ app/src/main/java/com/example/aveek/advancedrecyclerview/model/ItemTemplate.java
@@ -0,0 +1,49 @@
+package com.example.aveek.advancedrecyclerview.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aveek on 24/02/2017.
+ */
+
+public class ItemTemplate {
+    private int id;
+    private String itemTitle;
+    private String itemType;
+    private ArrayList<SubItemTemplate> subItemList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemTitle() {
+        return itemTitle;
+    }
+
+    public void setItemTitle(String itemTitle) {
+        this.itemTitle = itemTitle;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public ArrayList<SubItemTemplate> getSubItemList() {
+        return subItemList;
+    }
+
+    public void setSubItemList(ArrayList<SubItemTemplate> subItemList) {
+        this.subItemList = subItemList;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/aveek/advancedrecyclerview/model/SubItemTemplate.java
@@ -0,0 +1,37 @@
+package com.example.aveek.advancedrecyclerview.model;
+
+/**
+ * Created by Aveek on 24/02/2017.
+ */
+
+public class SubItemTemplate{
+
+    private int subItemId;
+    private String subItemTitle;
+    private String subItemDescription;
+
+    public int getSubItemId() {
+        return subItemId;
+    }
+
+    public void setSubItemId(int subItemId) {
+        this.subItemId = subItemId;
+    }
+
+    public String getSubItemTitle() {
+        return subItemTitle;
+    }
+
+    public void setSubItemTitle(String subItemTitle) {
+        this.subItemTitle = subItemTitle;
+    }
+
+    public String getSubItemDescription() {
+        return subItemDescription;
+    }
+
+    public void setSubItemDescription(String subItemDescription) {
+        this.subItemDescription = subItemDescription;
+    }
+
+}
